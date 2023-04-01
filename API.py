eb8067c92732a75b4e1400a900212033be63ad21
casapudenis <alotdenis@gmail.com>
2023-04-01 12:02:41 +0300

    API.py

---
+++ API.py
@@ -0,0 +1,8 @@
+import requests
+import json
+response_API = requests.get('https://cdn.cs.trade:2096/api/prices_CSGO')
+print(response_API.status_code)
+data = response_API.text
+parse_json=json.loads(data)
+with open('output_file.json', 'w') as f:
+    json.dump(parse_json, f)
